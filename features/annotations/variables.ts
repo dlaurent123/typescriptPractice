6f4a14a8bd75c43f129336c06d60e3fd2db30c52
dannyl <danilovlaurent@pursuit.org>
2021-04-12 16:22:33 -0400

    complete type annotation section

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -25,3 +25,23 @@ let point: {x:number; y:number} = {
     x:10,
     y:20
 }
+
+
+// Type annotation for functions
+
+const logNumber: (i:number) => void = (i:number) =>{
+    console.log(i)
+}
+
+// when to use annotations
+/////////
+// 1.function that returns type any
+
+const json = '{"x":10, "y":"20"}'
+
+const coordinates: {x:number; y:number}  = JSON.parse(json)
+
+
+const add = (a:number, b:number): number =>{
+    return a + b
+}
