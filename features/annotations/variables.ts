90046494938ceec0163b3abd49cfdb46d04c720f
dannyl <danilovlaurent@pursuit.org>
2021-04-12 15:22:47 -0400

    asigning variables with type annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,27 @@
+let apples: number = 5
+
+let speed: string = "fast"
+
+let hasName: boolean = true
+
+let nothingMuch : null = null
+
+let nothing : undefined = undefined
+
+// built in objects
+
+let now: Date = new Date()
+
+// array 
+// below represents a type annotation that states the type is an array with strings
+let colors: string[] = ["red", "green","blue"]
+
+let myNumbers: number[] = [1,2,3]
+
+let test: object[] = [{yes:1}]
+
+// object litteral
+let point: {x:number; y:number} = {
+    x:10,
+    y:20
+}
